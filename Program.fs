5ce8414966ad1290e447f3a853f29e129b0b9601
Dan Mannock <dmannock@users.noreply.github.com>
2020-04-17 22:02:47 +0100

    stream category matching, event handler, notes rmeoved, cleaned up

--- Program.fs
+++ Program.fs
@@ -1,20 +1,13 @@
-﻿//Gave up running in fsi after adding these & needing more
-//NOTE: 11.0.2 ver
-// #r @"C:\Users\Dan\.nuget\packages\newtonsoft.json\11.0.2\lib\netstandard2.0\Newtonsoft.Json.dll"
-// #r @"C:\Users\Dan\.nuget\packages\fscodec\2.0.1\lib\netstandard2.0\FsCodec.dll"
-// #r @"C:\Users\Dan\.nuget\packages\fscodec.newtonsoftjson\2.0.1\lib\netstandard2.0\FsCodec.NewtonsoftJson.dll"
-
-open System
-open Newtonsoft.Json
-open Newtonsoft
+﻿open System
 open FsCodec
+open FsCodec.Core
 open FsCodec.NewtonsoftJson
 
 let inline des<'t> x = Serdes.Deserialize<'t> x
 
 module Events =
-//TODO: check Guid types either JsonIsomorphism or tagged string (UMX)
-// using Newtonsoft wich already handles guids fine...
+    let [<Literal>] Category = "Product"
+
     type ProductSkuAdded = {
         Id: Guid
         Name: string
@@ -36,8 +29,18 @@ module Events =
         | UnhandledEvent
         interface TypeShape.UnionContract.IUnionContract
 
-    let codec = FsCodec.NewtonsoftJson.Codec.Create<Event>() 
+    let codec = NewtonsoftJson.Codec.Create<Event>() 
     let (|Decode|_|) = codec.TryDecode 
+
+    let (|InCategory|_|) =
+        function
+        | StreamName.CategoryAndId(Category, id) -> Some id
+        | _ -> None
+
+    let (|Match|_|) =
+        function
+        | InCategory(id), (Decode e) -> Some(id, e)
+        | _ -> None
 
 module ProductProjection =
     open Events
@@ -113,28 +116,34 @@ let prodAddEvent2 = """
 
 let utf8 (s : string) = System.Text.Encoding.UTF8.GetBytes(s)
 let events = [
-    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", FsCodec.Core.TimelineEvent.Create(0L, "ProductSkuAdded", utf8 prodAddEvent)
-    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", FsCodec.Core.TimelineEvent.Create(1L, "ProductPriceAdjusted", utf8 prodPriceAdjustedEvent)
-    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", FsCodec.Core.TimelineEvent.Create(2L, "UnhandledEvent", utf8 "")
-    StreamName.parse "Product-c7718eda-e628-4633-a082-62ccd29c7c76", FsCodec.Core.TimelineEvent.Create(0L, "ProductSkuAdded", utf8 prodAddEvent2)
+    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", TimelineEvent.Create(0L, "ProductSkuAdded", utf8 prodAddEvent)
+    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", TimelineEvent.Create(1L, "ProductPriceAdjusted", utf8 prodPriceAdjustedEvent)
+    StreamName.parse "Product-5e29e373-d092-4bc0-94d9-3cf8e48ed5fb", TimelineEvent.Create(2L, "UnhandledEvent", utf8 "")
+    StreamName.parse "Product-c7718eda-e628-4633-a082-62ccd29c7c76", TimelineEvent.Create(0L, "ProductSkuAdded", utf8 prodAddEvent2)
+    StreamName.parse "Other-99999999-e628-4633-a082-62ccd29c7c76", TimelineEvent.Create(0L, "AnotherEvent", utf8 """{}""")
 ]
+
+let handleEvents events =
+    events
+    |> List.choose (fun (stream, e) ->
+        match stream, e with
+        | Events.Match(id, e) -> Some(id, e)
+        | StreamName.CategoryAndId(category, id), e -> 
+            printfn "Unhandled event Category=%s Id=%s Index=%d Event=%A CausationId=%s CorrelationId=%s"
+                category id e.Index e.EventType e.CausationId e.CorrelationId
+            None                
+        | _ ->
+            //simulate logging
+            printfn "Failed to deserialize StreamName=%s Index=%d Event=%A CausationId=%s CorrelationId=%s"
+                (StreamName.toString stream) e.Index e.EventType e.CausationId e.CorrelationId
+            None)
 
 [<EntryPoint>]
 let main argv =
     let e = prodAddEvent |> des<Events.ProductSkuAdded>
     printfn "Deserialized raw json to domain event. Options & casing handled:\n%A" e
     printfn "Decoded events:"
-    let decoded = 
-        events
-        //TODO: decoding error handling - pass in ILogger
-        |> List.choose (fun (stream, e) ->
-            match stream, e with
-            | StreamName.CategoryAndId(_, id), (Events.Decode e) -> Some(id, e)
-            | _ ->
-                //simulate logging
-                printfn "Failed to deserialize StreamName=%s Index=%d Event=%A CausationId=%s CorrelationId=%s"
-                    (StreamName.toString stream) e.Index e.EventType e.CausationId e.CorrelationId
-                None)
+    let decoded = events |> handleEvents
     let finalState = decoded |> List.fold ProductProjection.evolveSingle ProductProjection.initialState 
     printfn "Final state: %A" finalState
     0
